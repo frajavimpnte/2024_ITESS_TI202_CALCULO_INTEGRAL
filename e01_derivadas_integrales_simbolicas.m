7e1a079259f0a81206356cca4177320371739423
frajavimopu <frajavimopu@gmail.com>
2024-05-06 12:08:35 -0600

    inicio del repositorio calculo

---
+++ e01_derivadas_integrales_simbolicas.m
@@ -0,0 +1,8 @@
+%% ITESS-TICS
+%% Enero-Junio 2024
+%% TI202 CALCULO INTEGRAL
+%% Francisco Javier Montecillo Puente
+%% Derivadas e integrales simbolicas
+%% 06 mayo 2024
+pkg load symbolic
+syms x
