b9e2159c3c45e788b075714fe00bb016e27aac43
frajavimopu <frajavimopu@gmail.com>
2024-05-08 13:31:29 -0600

    ejemplo de una función, su derivada e integral

--- e01_derivadas_integrales_simbolicas.m
+++ e01_derivadas_integrales_simbolicas.m
@@ -6,3 +6,20 @@
 %% 06 mayo 2024
 pkg load symbolic
 syms x
+
+%% definir una funcion
+disp('una función:')
+fx = x * sin (x)
+
+%% derivar
+disp('derivada:')
+dfx = diff(fx, x)
+
+%% integrar indefinida
+disp('integral indefinida:')
+ifx = int(fx, x)
+
+%% integral definida
+disp('integral definida:')
+ifx = double (  int(fx, x, 0, 1) )
+
